08d38ccc105e39c2019b64c77a0700a086e9c618
slug001 <84364313+slug001@users.noreply.github.com>
2022-09-07 20:24:14 +0800

    Add files via upload

---
+++ itsa_3.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(){
+    int x,y;
+    while(scanf("%d %d",&x,&y)!=EOF){
+        if((x*x+y*y)<=10000){
+            printf("inside\n");
+        }else{
+            printf("outside\n");
+        }
+    }
+    return 0;
+}
